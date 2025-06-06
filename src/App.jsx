478b56e588a63e40e3144a2915006af0e0fc77d5
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-06-06 11:38:26 +0600

    tailwindcss installed and fake data created as well as fetch from Blogs components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+       <h2 className='text-yellow-300'>Knowledge</h2>
+       <Blogs></Blogs>
+      </div>
+    </>
+  )
+}
+
+export default App
